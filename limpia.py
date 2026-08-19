8e933ce2f7fea967046fca5cb9192966da64910d
CristianPB14 <cristian.14.camilo08@gmail.com>
2026-08-19 17:24:14 -0500

    Delete limpia.py

--- limpia.py
+++
@@ -1,8 +0,0 @@
-import re, glob
-pat = re.compile(r"[ \t]*\[cite:[^\]]*\]")
-for f in glob.glob("docs/*.md") + glob.glob("resultados/*.md"):
-    s = open(f, encoding="utf-8").read()
-    n = len(pat.findall(s))
-    if n:
-        open(f, "w", encoding="utf-8", newline="\n").write(pat.sub("", s))
-        print(f, n)
